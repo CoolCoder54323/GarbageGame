4060242347ea4f6c262edf8fe480381288e8337f
Dennis Dixon <dennis.dixon@Denniss-MacBook-Pro.local>
2018-02-04 10:53:08 -0500

    Some possible help

--- first ar project/ViewController.swift
+++ first ar project/ViewController.swift
@@ -11,19 +11,33 @@ import SceneKit//3d objects
 import ARKit
 
 
-
+struct Model {
+    var filename: String
+    var filter: String?
+}
 
 class MaterialText {
     var text: SCNText
     var material: SCNMaterial { return text.materials.first! }
-    
-    init(text: SCNText, material: SCNMaterial) {
+    private var currentNode: SCNNode?
+
+    init(text: SCNText, material: SCNMaterial, color: UIColor) {
         self.text = text
         self.text.materials = [material] //makes the objct into your text
+        self.text.materials.first?.diffuse.contents = color //turns it a color
     }
     
     func setColor(_ color: UIColor) {
         text.materials.first?.diffuse.contents = color //turns it a color
+    }
+
+    func node(scale: SCNVector3 = SCNVector3(0.2,0.2,0.2), at position: SCNVector3 = SCNVector3(0.0,0.0,-1.0)) -> SCNNode {
+        guard currentNode == nil else { return (self.currentNode)! }
+        let newNode = SCNNode(geometry: self.text)
+        newNode.scale = scale
+        newNode.position = position
+        self.currentNode = newNode
+        return newNode
     }
 }
 
@@ -35,62 +49,55 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var startButtonContainer: UIView!
     @IBOutlet weak var startContainerBottomConstraint: NSLayoutConstraint!
 
+    // We can just add the model file names to this array using the Model class and the load3DGarbageModels() function will add them to the scene
+    private var garbageModels = [Model(filename: "art.scnassets/chips-sticks-open.dae", filter: "stick")]
     private var isStartingGame = true
     private var garbageScene = SCNScene()
-    private let titleText = MaterialText(text: SCNText(string:"Garbage Game", extrusionDepth:0.7), material: SCNMaterial())
+    private let titleText = MaterialText(text: SCNText(string:"Garbage Game", extrusionDepth:0.7), material: SCNMaterial(), color: UIColor.green)
+    private let developerText = MaterialText(text: SCNText(string:"By:inVeNT", extrusionDepth:0.7), material: SCNMaterial(), color: UIColor.orange)
+
+    let cigarette = SCNNode(geometry: nil)
 
     //runs when view loads
     override func viewDidLoad() {
         super.viewDidLoad()
-   
-        // Set the view's delegate
+
+        // Create a new scene and set the scene to the view,
+        // set the scene view's delegate, show statistics, and debug info
+        sceneView.scene = SCNScene()
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-      
         sceneView.showsStatistics = true
         sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
-    
-        titleText.setColor(UIColor.green)
-        
-        
+        // sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+
+        // We were trying to add the ship to the scene directly here
+        // but found out that we have to use this method OR the method of adding an anchor
+        // to the sceneView.session and then return the corresponding node
+        // in the "renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor)" function below
+//        let nodeModel:SCNNode!
+//        let nodeName = "ship"
+//        let modelScene = SCNScene(named : "ship.scn")!
+//        nodeModel =  modelScene.rootNode.childNode(withName: nodeName, recursively: true)
+//        sceneView.scene.rootNode.addChildNode(cigarette)
+
         showStartButtonContainer(false, animated: false)
         startButton.setTitle("START THE GAME", for: .normal)
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-       
-        // Create and pass a text and a material object into our custom class
-        titleText.setColor(UIColor.green)
-
-        let nameNode = SCNNode(geometry: titleText.text)
-        nameNode.position = SCNVector3(x: -0.05, y: 0.2, z: -3)
-        nameNode.scale = SCNVector3(x: 0.05, y: 0.05, z: 0.1)
-
-        let developerText = MaterialText(text: SCNText(string:"By:inVeNT", extrusionDepth:0.7), material: SCNMaterial())
-        developerText.setColor(UIColor.orange)
-        
-        let developerNode = SCNNode(geometry: developerText.text)
-        developerNode.position = SCNVector3(x: -0.05, y: -0.25, z:-3)
-        developerNode.scale = SCNVector3(x: 0.05, y: 0.05, z: 0.1)
 
         sceneView.scene = garbageScene
-        sceneView.scene.rootNode.addChildNode(nameNode)
-        sceneView.scene.rootNode.addChildNode(developerNode)
+        sceneView.scene.rootNode.addChildNode(titleText.node(scale: SCNVector3(x: 0.01, y: 0.01, z: 0.05), at: SCNVector3(x: -0.05, y: 0.2, z: -3)))
+        sceneView.scene.rootNode.addChildNode(developerText.node(scale: SCNVector3(x: 0.01, y: 0.01, z: 0.05), at: SCNVector3(x: -0.05, y: 0.0, z:-3)))
         sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
 
-        // Run the view's session
+        // Create a session configuration and run the view's session
+        let configuration = ARWorldTrackingConfiguration()
         sceneView.session.run(configuration)
+
+        // Add the ship anchor and node only when we want to test putting the airplane model in the scene
+        // createShip()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -104,6 +111,81 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
+    @IBAction func startButtonPressed(_ sender: Any) {
+        sceneView.scene.rootNode.childNodes.forEach { (child) in
+            child.removeFromParentNode()
+        }
+
+        load3DGarbageModels(garbageModels)
+        showStartButtonContainer(false, animated: true)
+    }
+
+    // This function is a way to load all the models we add to the project
+    func load3DGarbageModels(_ models: [Model]) {
+        var newXPosition = 0.0
+        models.forEach { (model) in
+            guard let modelScene = SCNScene(named: model.filename) else { return }
+            // The next line is a VERY basic way to position the objects sequentially along the x axis but a fixed y coordinate
+            // and should be improved later to logically determine the horizontal surfaces and more intelligently
+            // position the nodes representing the models
+            let positionVector = SCNVector3(newXPosition,-1.0,-1.0)
+            if let newNode = scaledNode(from: modelScene, at: positionVector, scale: SCNVector3(0.8, 0.8, 0.8), filteredName: model.filter ) {
+                sceneView.scene.rootNode.addChildNode(newNode)
+            }
+            newXPosition += 1.0
+        }
+    }
+
+    // This allows us to extract and filter out the parts of the model, scale them, and create an SCNNode from them to put in the scene
+    func scaledNode(from scene: SCNScene, at position: SCNVector3, scale: SCNVector3 = SCNVector3(1.0, 1.0, 1.0), filteredName: String? = nil) -> SCNNode? {
+        guard !scene.rootNode.childNodes.isEmpty else { return nil }
+
+        let node = SCNNode()
+        node.scale = scale
+        node.position = position
+
+        scene.rootNode.childNodes
+            .filter {
+                guard let name = $0.name else { return false }
+                guard let filteredName = filteredName else { return true }
+                return !name.contains(filteredName)
+            }
+            .forEach {
+                print("adding node ", $0, " from the model file to the new node")
+                $0.scale = SCNVector3(1.0, 1.0, 1.0)
+                node.addChildNode($0)
+            }
+        return node
+    }
+    
+    // MARK: - ARSCNViewDelegate functions
+
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        showError(title: "Sorry", message: "The AR session wasn't able to start. Please stop the app and try again.")
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        showError(title: "Sorry", message: "The AR session was interrupted. Please stop the app and try again.")
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        showError(title: "Sorry", message: "The AR session ended. Please stop the app and try again.")
+    }
+
+    // MARK: - Helper Functions
+
+    // This gives us an easy way to put up an error message on the screen
+    func showError(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
+    // This gives us an easy way to show and hide the start game button
     func showStartButtonContainer(_ shouldShow: Bool, animated: Bool) {
         let duration = animated ? 1.0 : 0.0
         UIView.animate(withDuration: duration, animations: { [weak self] in
@@ -113,47 +195,37 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             self?.view.layoutIfNeeded()
         })
     }
-    
-    @IBAction func startButtonPressed(_ sender: Any) {
-        sceneView.scene.rootNode.childNodes.forEach { (child) in
-            child.removeFromParentNode()
-        }
-        // Chip sticks
-        if let chipSticks = SCNScene(named: "art.scnassets/chips-sticks-open.dae") {
-            sceneView.scene = chipSticks
-            showStartButtonContainer(false, animated: true)
-            adjustNodes()
-        }
-    }
-    
-    func adjustNodes() {
-        sceneView.scene.rootNode.childNodes.forEach {
-            print("node: ", $0)
-//            $0.scale = SCNVector3(0.1, 0.1, 0.1)
-        }
-    }
-    
-    // MARK: - ARSCNViewDelegate
-/*
-    Override to create and configure nodes for anchors added to the view's session.
+
+    // MARK: - Functions we're not using now
+
+    // This next ARSCNViewDelegate function that for now works only when the createShip() function is called
+    // -- when the anchor is added to the session, this method is called to return a node
+    // right now we're not using it
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+        let boxGeometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.0)
+        boxGeometry.firstMaterial?.diffuse.contents = UIImage(named: "art.scnassets/ship.scn")
+        let boxNode = SCNNode(geometry: boxGeometry)
+        boxNode.position = SCNVector3(0, 0, -1.5)
+        return boxNode
     }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+
+
+    // Our test helper function that we're not using now
+    func createShip(){
+        guard let cameraTransform = sceneView.session.currentFrame?.camera.transform else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: { [weak self] in
+                self?.createShip()
+            })
+            return
+        }
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -1.0
+        let shipTransform = cameraTransform * translation
+        let ship = ARAnchor(transform: shipTransform)
+        sceneView.session.add(anchor: ship)
     }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-    }
+
+
 }
 
 
